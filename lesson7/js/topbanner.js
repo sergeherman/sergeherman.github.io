9ccb6ab702139607abb7f6da2d09d4d5b7adac22
Use your GitHub usernamesergeherman <sergeherman@byui.edu>
2019-11-03 00:02:19 +0300

    lesson 7 update

---
+++ lesson7/js/topbanner.js
@@ -0,0 +1,10 @@
+var d = new Date();
+var n = d.getDay()
+           if ( n == 5) {
+              
+                  document.getElementById('topbanner').innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion." ;
+              }  
+              
+
+
+
